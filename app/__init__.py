c2b5b827925ecd197173ea755d60c466b5cfc486
winter0019 <121707096+winter0019@users.noreply.github.com>
2025-08-03 13:57:01 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager
 
 # Initialize extensions outside of the factory function
 db = SQLAlchemy()
@@ -27,24 +27,23 @@ def create_app():
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default-secret-key')
-    
+
     db.init_app(app)
     bcrypt.init_app(app)
     csrf.init_app(app)
     login_manager.init_app(app)
-
+    
     # The Flask-Login user loader
     @login_manager.user_loader
     def load_user(user_id):
         from .models import User
         return User.query.get(int(user_id))
 
+    # Import the models and routes
+    from . import models, routes
+
     with app.app_context():
-        # Import the models and routes
-        from . import models, routes
-        
         # Create database tables if they don't exist
         db.create_all()
 
     return app
-
